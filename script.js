cbc62637e5dab1500f32880b0b4741f65b703e5b
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-10 16:39:21 -0300

    Estrutura de Repetição

---
+++ script.js
@@ -0,0 +1,19 @@
+var colors =["black","white","red","green","blue"]
+
+for (var i = 0;i<colors.length;i++){
+    console.log(colors[i]);
+}
+
+for(i=1;i<=10;i++){
+    console.log(9*i);
+}
+
+i=1;
+while(i<=10){
+    console.log(8*i);
+    i++;
+}
+
+do{
+    console.log(8*i);
+}while(i<1)
