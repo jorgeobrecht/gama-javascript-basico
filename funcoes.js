fa233ffc92ccf01572c3647b8a50e0510f244a0a
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-10 16:44:27 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function sayHello(name,lastName){
+    console.log('Hello '+ name + " " + lastName)
+}
+
+sayHello("Jorge","Obrecht");
+
+function sum(x,y){
+    return x+y
+}
+
+console.log(sum(10,15));
