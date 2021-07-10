f3a9de3ff79be022ac3bcd0166613d784f28d2b3
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-10 15:59:37 -0300

    Tipos de dados

---
+++ dados.js
@@ -0,0 +1,24 @@
+var myName="Jorge" // string 
+
+var age = 25 //number-int
+
+var height = 1.73 //number-float
+
+null //objeto que nao possui dados
+
+undefined//dado nao definido
+
+var isStudent= true//boolean
+
+var person = {  //Objeto
+    "name":"Jorge",
+    "age":25
+}
+
+var students=["Jorge","Joao","Maria"] //array
+
+function sum(a,b){ //funcao
+    return a+b
+}
+
+console.log(typeof sum)//typeof diz o tipo
