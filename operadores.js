5f22bf0308f24cc3cef9fb2e51f71b652b49c117
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-10 16:17:21 -0300

    Operadores

---
+++ operadores.js
@@ -0,0 +1,38 @@
+// Operadores aritmeticos
+var x = 4
+var y = 2
+//adicao +
+console.log(x+y)
+//subtracao -
+console.log(x-y)
+//multiplicacao *
+console.log(x*y)
+//divisao /
+console.log(x/y)
+//modulo ou resto de um divisao %
+console.log(x%y)
+//incremento ++
+console.log(++x)
+//decremento -- */
+console.log(--y)
+
+//operador de atribuicao
+//= atribuicao
+
+console.log(x+=y)
+console.log(x-=y)
+console.log(x*=y)
+console.log(x/=y)
+console.log(x%=y)
+
+//operadores de comparacao 
+//== compara valores
+//=== comparar valores e tipo
+//!= diferente de 
+//< ou <=
+//> ou >=
+
+//operadores logicos
+//e &&
+//ou ||
+//nao !
---
+++ variaveis.js
@@ -0,0 +1,22 @@
+console.log("Hello World")
+
+var myName="Jorge"; /* escopo global   */
+/* Formato camelCas, n pode colocar numero no comeco mas pode simbolo que nao eh recomendado */
+
+let language ="Javascript"; /* escopo de funcao  */
+
+const pattern="ECMAScript"; /* escopo de funcao que nao pode ser mudado  */
+
+console.log(myName)
+
+{
+    var age= 25;
+}
+
+console.log(age);
+
+{
+    let idade =26;
+}
+
+console.log(idade);
